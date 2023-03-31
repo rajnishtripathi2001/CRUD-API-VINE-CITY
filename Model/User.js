7d316b810072aa0debc528e08c90362a729b2592
Rajnish <rajnishtripathi2001@gmail.com>
2023-04-01 02:06:23 +0530

    mongodb schema modified

--- Model/User.js
+++ Model/User.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:String,
+    fname:String,
+    lname:String,
     email:String,
-    origin:String,
+    country:String,
+    username:String,
+    password:String,
     category:String,
     VID:String,  //Vinish ID
     ResID:String, //Resident ID
